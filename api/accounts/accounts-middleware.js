95e9ed8310b5ea7be12ab9d42f8a6b9f286bff32
ssamin225 <sadmansamin225@gmail.com>
2021-12-07 11:17:25 -0500

    completed mvp

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -5,18 +5,18 @@ exports.checkAccountPayload = (req, res, next) => {
   const { name, budget } = req.body
   if (!name || !budget) {
     next({ status: 400, message: 'name and budget are required!' })
-  } else if (typeof name !== "string") {
-    next({ status: 400, message: "name of account must be a string" });
-  } else if (typeof budget !== "number") {
-    next({ status: 400, message: "budget of account must be a number" });
-  } else if (name.trim()) {
-    next({ status: 400, message: "name and budget are required" });
+  } else if (typeof name !== 'string') {
+    next({ status: 400, message: 'name of account must be a string' });
+  } else if (!parseInt(budget)) {
+    next({ status: 400, message: 'budget of account must be a number' });
+  } else if (!name.trim()) {
+    next({ status: 400, message: 'name and budget are required' });
   } else if (3 > name.trim().length  || name.trim().length > 100) {
-    next({ status: 400, message: "name of account must be between 3 and 100" });
-  } else if(budget < 0 || budget >1000000){
-    next({ status: 400, message: "budget of account is too large or too small"  });
+    next({ status: 400, message: 'name of account must be between 3 and 100' });
+  } else if(budget < 0 || budget > 1000000){
+    next({ status: 400, message: 'budget of account is too large or too small'  });
   } else {
-    name = name.trim()
+    req.body.name = name.trim()
     next();
   }
 }
